e0905c33adf86b5e9bf8439bb9ad412fd87313ed
Brenna Darroch <bcmdarroch@gmail.com>
2017-01-08 13:25:11 -0800

    Update election.rb
    Updated with version I submitted for Pre-Ada hw
    
    -added display of 1 "vote" vs plural "votes"
    -added case for ties

--- election.rb
+++ election.rb
@@ -1,19 +1,23 @@
+
 puts "Welcome to my election voting program!"
 
- puts "The election candidates are: Link, Ganondorf, Zelda"
+puts "How many voters are there?" #handle more than 10 votes
+numvoters = gets.chomp.to_i
+
+puts "The election candidates are: Zelda, Link, and Ganondorf"
 
 l=0
 z=0
 g=0
 
-10.times do
+numvoters.times do
   puts "Who do you vote for?"
   vote = gets.chomp
-    if vote == "Link" || vote == "link"
+    if vote.downcase == "link"
       l = l+1
-    elsif vote == "Zelda" || vote == "zelda"
+    elsif vote.downcase == "zelda"
       z = z+1
-    elsif vote == "Ganondorf" || vote == "ganondorf"
+    elsif vote.downcase == "ganondorf" || vote.downcase == "ganon"
       g = g+1
     end
 
@@ -22,17 +26,30 @@ end
 puts "ELECTION RESULTS..."
 
 puts "Vote Summary:"
+
 print "Link: " 
 print l 
-puts" vote(s)"
+if l > 1 #handle grammer of vote summary
+  puts " votes" 
+else
+  puts " vote"
+end
 
 print "Ganondorf: "
 print g
-puts " vote(s)"
+if g > 1
+  puts " votes"
+else
+  puts " vote"
+end
 
 print "Zelda: "
 print z
-puts " vote(s)"
+if z > 1
+  puts " votes"
+else
+  puts " vote"
+end
 
 if l > z && l > g
   puts "Winner: Link!"
@@ -40,4 +57,12 @@ elsif g > l && g > z
   puts "Winner: Ganondorf!"
 elsif z > l && z > g
   puts "Winner: Zelda!"
+elsif l == z && l > g #handle ties
+  puts "Tied Winners: Link and Zelda!"
+elsif l == g && l > z
+  puts "Tied Winners: Link and Ganondorf!"
+elsif z == g && z > l
+  puts "Tied Winners: Zelda and Ganondorf!"
+elsif l == z && l == g
+  puts "Tied Winners: Link, Zelda, and Ganondorf!"
 end
